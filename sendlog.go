4f121d81f898dcd61b08cab7b79d02c26a094da3
islamyakin <nurulyakin79@gmail.com>
2023-12-03 01:02:26 +0700

    refactor: exit(1)

--- sendlog.go
+++ sendlog.go
@@ -2,36 +2,40 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"github.com/confluentinc/confluent-kafka-go/v2/kafka"
 	"log"
 	"os"
 )
 
-func sendLogLines(logFile string, producer *kafka.Producer, topic string) {
+func sendLogLines(logFile string, producer *kafka.Producer, topic string) error{
 	file, err := os.Open(logFile)
 	if err != nil {
 		log.Fatalf("Failed to open log file: %s", err)
 	}
-	defer file.Close()
+	defer func(file *os.File) {
+		err := file.Close()
+		if err != nil {
+			log.Fatalf("Failed to close log file: %s", err)
+		}
+	}(file)
 
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		line := scanner.Text()
-		producer.Produce(&kafka.Message{
+		err := producer.Produce(&kafka.Message{
 			TopicPartition: kafka.TopicPartition{Topic: &topic, Partition: kafka.PartitionAny},
 			Value:          []byte(line),
 		}, nil)
-	}
-	if err != nil {
-		log.Printf("Failed to send message to Kafka: %s\n", err)
-	} else {
-		log.Printf("Message sent to Kafka %s", producer.String())
+		if err != nil {
+			return fmt.Errorf("failed to send message to Kafka: %w", err)
+		}
 	}
 	if err := scanner.Err(); err != nil {
-		log.Printf("Error reading log file: %s", err)
+		return fmt.Errorf("error reading log file: %w", err)
 	}
 
 	// Tunggu pengiriman pesan selesai
 	producer.Flush(15 * 1000)
-
+return nil
 }
